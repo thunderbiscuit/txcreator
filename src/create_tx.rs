f7c10daa2bb824c8beb154c3d5b5062de6be3927
thunderbiscuit <thunderbiscuit@protonmail.com>
2022-06-07 11:19:51 -0300

    Add working cli

--- src/create_tx.rs
+++ src/create_tx.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
+use std::str::FromStr;
 use bdk::{bitcoin, FeeRate, SignOptions, SyncOptions, Wallet};
-use bdk::bitcoin::Transaction;
-// use bdk::bitcoin::Network;
-// use bdk::bitcoin::util::address::Payload::WitnessProgram;
+use bdk::bitcoin::{Address, Transaction};
+use bdk::bitcoin::util::psbt::serialize::Serialize;
 use bdk::blockchain::ElectrumBlockchain;
 use bdk::database::MemoryDatabase;
 use bdk::electrum_client::Client;
+use bitcoin_bech32::WitnessProgram;
 use hex::FromHex;
-use hex::ToHex;
 
 pub fn create_tx(
     descriptor: String, 
@@ -30,48 +30,42 @@ pub fn create_tx(
         MemoryDatabase::default(),
     )?;
 
+    // testing that the wallet gets created and can sync
+    // let address = wallet.get_address(AddressIndex::New).unwrap().to_string();
     wallet.sync(&blockchain, SyncOptions::default())?;
+    // let balance = wallet.get_balance().unwrap().to_string();
 
-    let output_script: Vec<u8> = Vec::from_hex(output_script_hex).unwrap();
+    let output_script_raw: Vec<u8> = Vec::from_hex(&output_script_hex).expect("Transforming the hex into raw bytes didn't work");
 
-    // let _addr = WitnessProgram::from_scriptpubkey(
-    //     &output_script[..],
-    //     match network {
-    //         Network::Bitcoin => panic!("Mainnet unsupported"),
-    //         Network::Testnet => bitcoin_bech32::constants::Network::Testnet,
-    //         Network::Regtest => bitcoin_bech32::constants::Network::Regtest,
-    //         Network::Signet  => panic!("Signet unsupported"),
-    //     },
-    // )
-    // .expect("Lightning funding tx should always be to a SegWit output")
-    // .to_address();
+    let address = WitnessProgram::from_scriptpubkey(
+        &output_script_raw[..],
+        bitcoin_bech32::constants::Network::Testnet
+    )
+        .expect("Lightning funding tx should always be to a SegWit output")
+        .to_address();
+
+    // thread 'main' panicked at 'Deserialization didn't work: ParseFailed("data not consumed entirely when explicitly deserializing")'
+    // let output_script: Script = deserialize(&output_script_raw).expect("Deserialization didn't work");
 
     let fee_rate = FeeRate::from_sat_per_vb(2.0);
 
-    // let mut tx_builder = wallet.build_tx();
-    // tx_builder
-    //     .add_recipient(output_script.clone(), channel_value_satoshis)
-    //     .fee_rate(fee_rate)
-    //     .enable_rbf();
-    // let mut psbt = tx_builder.finish().unwrap();
-
-    let (mut psbt, tx_details) = wallet.build_tx()
-        .add_recipient(output_script.clone(), channel_value_satoshis)
-        .fee_rate(fee_rate)
-        .enable_rbf()
-        .finish()
-        .unwrap();
+    let (mut psbt, details) = {
+        let mut builder = wallet.build_tx();
+        builder
+            // .add_recipient(address.script_pubkey(), channel_value_satoshis)
+            .add_recipient(Address::from_str(address.as_str()).unwrap().script_pubkey(), channel_value_satoshis)
+            .fee_rate(fee_rate)
+            .enable_rbf();
+        builder
+            .finish()?
+    };
 
     wallet.sign(&mut psbt, SignOptions::default());
 
-    let mut funding_tx: Transaction = psbt.extract_tx();
+    let funding_tx: Transaction = psbt.extract_tx();
+    let funding_tx_encoded = funding_tx.serialize();
 
-    let mut funding_tx_hex = String::new();
+    let tx = hex::encode(&funding_tx_encoded);
 
-    // serialize not found in bdk::bitcoin::Transaction
-    // funding_tx_hex.write_hex(&mut funding_tx.serialize());
-
-    funding_tx_hex.write_hex(&mut funding_tx);
-
-    return Ok(funding_tx_hex);
+    return Ok(tx);
 }
